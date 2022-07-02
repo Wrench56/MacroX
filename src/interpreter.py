8450908d88fc770721907938d09bee0a6cd4c2d3
Wrench56 <dmarkreg@gmail.com>
2022-07-03 00:41:31 +0200

    Added automatic interpreter which executes the different commands from the imported command files (when it matches the template)

---
+++ src/interpreter.py
@@ -0,0 +1,23 @@
+from kwarg_converter import convert
+
+class Interpreter():
+    def __init__(self, commands: dict) -> None:
+        self.commands = commands
+
+    def execute(self, command):
+        parsed = convert(command=command)
+        print(parsed)
+        command = parsed.get(0)
+
+        if command == None: # Empty line
+            return True
+        
+        command = command.lower()
+        if command in self.commands.keys():
+            module = self.commands[command]
+            obj = getattr(module, command.capitalize())
+            obj.execute(parsed)
+
+
+
+
