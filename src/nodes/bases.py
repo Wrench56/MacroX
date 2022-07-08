4298aa1b7f737c77e9f7f18a6c2387813d38678d
Wrench56 <dmarkreg@gmail.com>
2022-07-08 06:54:22 +0200

    Added BaseNode() class with node prettyprint (JSON-like)

---
+++ src/nodes/bases.py
@@ -0,0 +1,23 @@
+class BaseNode():
+    KIND = 'BaseNode'
+    _pp_left = 'left'
+    _pp_right = 'right'
+    def __init__(self):
+        pass
+    
+    def __repr__(self) -> str:
+        return self.prettyprint(0)
+    
+    def prettyprint(self, indent: int) -> str:
+        indent_str = ' '*indent
+        left = self.left
+
+        right = self.right
+        if isinstance(right, BaseNode):
+            right = right.prettyprint(indent=(indent+4))
+
+
+        return f'{self.KIND} (\n{indent_str}    {self._pp_left}: {left}\n{indent_str}    {self._pp_right}: {right}\n{indent_str})'
+
+    
+
