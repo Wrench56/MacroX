30dcaca3d82842aafc8bf9e94f143adef6e97931
Wrench56 <dmarkreg@gmail.com>
2022-07-08 06:49:22 +0200

    Added parser & refactoring (added method returns)

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,19 +1,25 @@
 from utils.kwarg_converter import convert
 from utils import prettyprint
 import tokenizer
+import parser
 class Interpreter():
     def __init__(self, path: str) -> None:
         self.path = path
 
     def start(self):
-        tokens = self.tokenize()
+        tokens = self.tokenize(self.path)
         prettyprint.print_tokens(tokens)
+        print('\n'*2)
 
-        
+        nodes = self.parse(tokens)
+        print(nodes)
 
-    def tokenize(self):
+    def tokenize(self, path):
         Tokenizer = tokenizer.Tokenizer()
-        return Tokenizer.tokenize(path=self.path)
+        return Tokenizer.tokenize(path=path)
 
+    def parse(self, tokens):
+        Parser = parser.Parser(tokens)
+        return Parser.parse()
 
 
