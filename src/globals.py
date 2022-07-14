8c6187b74e72ef46612f74b66eb5ccfa7c64d92d
Wrench56 <dmarkreg@gmail.com>
2022-07-14 09:29:21 +0200

    Added GlobalArgumentHandler() to handle all globally specified call arguments

--- src/globals.py
+++ src/globals.py
@@ -1,7 +1,8 @@
-from utils import variable_handler, jump_handler, importer
+from utils import variable_handler, jump_handler, importer, global_argument_handler
 
 VH = variable_handler.VariableHandler()
 JH = jump_handler.JumpHandler()
+GAH = global_argument_handler.GlobalArgumentHandler(vh=VH)
 Importer = importer.Importer()
 Importer.import_module('commands/win32api')
 
