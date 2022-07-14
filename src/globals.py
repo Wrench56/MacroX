e62c23c24cea4176192d593d76e2fa88853368e6
Wrench56 <dmarkreg@gmail.com>
2022-07-14 05:05:03 +0200

    Added default "commands" module import

--- src/globals.py
+++ src/globals.py
@@ -3,5 +3,6 @@ from utils import variable_handler, jump_handler, importer
 VH = variable_handler.VariableHandler()
 JH = jump_handler.JumpHandler()
 Importer = importer.Importer()
+Importer.import_module('commands')
 
 break_bool = False
