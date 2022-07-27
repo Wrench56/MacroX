1a7bcb1e20c13d4ad0b17f4d52e02ed1022d2109
Wrench56 <dmarkreg@gmail.com>
2022-07-27 08:25:05 +0200

    Added variable debugging

--- src/utils/debugger.py
+++ src/utils/debugger.py
@@ -11,3 +11,14 @@ def print_threads():
     logger.info(39*'=')
 
     globals.IM.resume_all()
+
+def print_vars():
+    globals.IM.pause_all()
+
+    vars = globals.VH.variables
+    logger.info(14*'=' + ' Variables ' + 14*'=')
+    for i, key in enumerate(vars):
+        logger.info(f'[{i}] - {key} : {vars[key]}')
+    logger.info(39*'=')
+    
+    globals.IM.resume_all()
